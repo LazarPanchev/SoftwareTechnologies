0dd8f9d9884aaa09b5216be87d1a863664636c12
LazarPanchev <l.panchev@abv.bg>
2018-04-19 22:42:35 +0300

    Add first Exam preparation Software Technologies

---
+++ EXAM1/Java/src/main/java/projectrider/bindingModel/ProjectBindingModel.java
@@ -0,0 +1,56 @@
+package projectrider.bindingModel;
+
+public class ProjectBindingModel {
+
+    public ProjectBindingModel() {
+    }
+
+    public ProjectBindingModel(Integer id, String title, String description, Integer budget) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.budget = budget;
+    }
+
+    private Integer id;
+
+    private String title;
+
+
+    private String description;
+
+
+    private Integer budget;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Integer budget) {
+        this.budget = budget;
+    }
+}
